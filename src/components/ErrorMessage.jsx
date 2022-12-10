11234af8766c8a6b0e22d2476e0ece3778e6da27
Jsh3ng <tanjs41200@gmail.com>
2022-12-10 21:41:59 +0800

    Create error message component

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+const ErrorMessage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+      <div id="error-page">
+          <h1>Oh no!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+              <i>{error.statusText || error.message}</i>
+          </p>
+      </div>
+  );
+}
+
+export default ErrorMessage;
