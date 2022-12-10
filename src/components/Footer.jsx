b91d7f10462367c8d5712dcf0c742af65462a237
Jsh3ng <tanjs41200@gmail.com>
2022-12-10 21:42:25 +0800

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { Layout } from "antd";
+import React from "react";
+
+const { Footer } = Layout;
+
+const FooterComponent = () => {
+  return (
+    <Footer
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      Jsask ©2022 Created by Tan Jing Sheng
+    </Footer>
+  );
+};
+
+export default FooterComponent;
