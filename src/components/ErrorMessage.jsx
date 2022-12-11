4f8ed6f28721956c1c2b9ee62191810cb7d7cda8
Jsh3ng <tanjs41200@gmail.com>
2022-12-12 00:30:37 +0800

    Format ErrorMessage.jsx

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -6,14 +6,14 @@ const ErrorMessage = () => {
   console.error(error);
 
   return (
-      <div id="error-page">
-          <h1>Oh no!</h1>
-          <p>Sorry, an unexpected error has occurred.</p>
-          <p>
-              <i>{error.statusText || error.message}</i>
-          </p>
-      </div>
+    <div id="error-page">
+      <h1>Oh no!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
   );
-}
+};
 
 export default ErrorMessage;
