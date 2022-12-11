77e6023bb2cbbd1bcd7ea91961be967a7bba2efe
Jsh3ng <tanjs41200@gmail.com>
2022-12-11 12:51:52 +0800

    Add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,125 @@
-import { Layout, Menu } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Col, Input, Layout, Menu, Row } from "antd";
 import React from "react";
 
+const {  useState  } = React;
+const { Search } = Input;
 const { Header } = Layout;
+const headerHeight = 60;
+
+const Logo = () =>{
+  return (
+    <>
+      <img src="..\LogoYellow.svg" alt="LogoYellow"
+        style={{
+          float: "left",
+          height: headerHeight,
+          padding: "8px 0 8px 0",
+          
+        }}/>
+      <img src="..\LogoWord.svg"  alt="LogoWord"
+        style={{
+          float: "left",
+          height: headerHeight,
+          padding: "16px 0px 10px 0px",
+        }}/>
+    </>
+  )
+}  
+
+
+const SearchBox = () =>{
+  const onSearch = (value) => console.log(value);
+  return (
+    <Search 
+      size="large"
+      placeholder="Search Jsask"
+      allowClear
+      onSearch={onSearch}
+      style={{
+        float: "left",
+        height: headerHeight,
+        padding: "10px 0px 10px 0px",
+        background: "rgba(255, 255, 255, 0.2)",
+      }}
+    />
+  )
+}  
+
+
+const AvatarIcon = () => {
+  return (
+  <Avatar 
+        size= "large"
+        icon={<UserOutlined />} 
+        shape= "square"
+        style={{
+          float: "right",
+          margin: "10px 0px 10px 0px",
+        }}  
+      />
+  )
+}
+
+
+const ProfileMenu = () => {
+  const [current, setCurrent] = useState("mail");
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  }
+  const items = [
+    {
+      label: "Bobby Lee",
+      key: "Menu",
+      children: [
+        {
+          label: "Profile"
+        },
+        {
+          label: "Logout"
+        },
+      ],
+    }];
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+      style={{
+        float: "left",
+        height: headerHeight,
+      }} />
+  )
+};
+
 
 const HeaderComponent = () => {
   return (
     <Header
-    style={{
-      position: 'sticky',
-      top: 0,
-      zIndex: 1,
-      width: '100%',
-    }}
-  >
-    <div
       style={{
-        float: 'left',
-        width: 120,
-        height: 31,
-        margin: '16px 24px 16px 0',
-        background: 'rgba(255, 255, 255, 0.2)',
+        position: "sticky",
+        top: 0,
+        zIndex: 1,
+        height: headerHeight,
+        width: "100%",
+        padding: "0px 20px 0px 20px",
+        backgroundColor: "white",
       }}
-    />
-    <Menu
-      theme="dark"
-      mode="horizontal"
-      defaultSelectedKeys={['2']}
-      items={new Array(3).fill(null).map((_, index) => ({
-        key: String(index + 1),
-        label: `nav ${index + 1}`,
-      }))}
-    />
-  </Header>
+    >
+
+    <Row>
+      <Col span={8}><Logo /></Col>
+      <Col span={8}><SearchBox /></Col>
+      <Col span={6}><AvatarIcon /></Col>
+      <Col span={2}><ProfileMenu /></Col>
+    </Row>
+
+    </Header>
   );
 };
 
 export default HeaderComponent;
+
