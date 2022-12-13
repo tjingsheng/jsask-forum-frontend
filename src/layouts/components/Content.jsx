431a2f1a79f278133ec780b2c032a83598c5410d
Jsh3ng <tanjs41200@gmail.com>
2022-12-13 18:25:40 +0800

    Force min height to fill up page

--- src/layouts/components/Content.jsx
+++ src/layouts/components/Content.jsx
@@ -3,6 +3,14 @@ import React from "react";
 
 const { Content } = Layout;
 
-const ContentComponent = ({ children }) => <Content>{children}</Content>;
+const ContentComponent = ({ children }) => (
+  <Content
+    style={{
+      minHeight: "calc(100vh - 127px)",
+    }}
+  >
+    {children}
+  </Content>
+);
 
 export default ContentComponent;
