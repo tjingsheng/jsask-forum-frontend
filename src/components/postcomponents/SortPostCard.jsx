595d663f70f58a40b107a7afa041291ddf434285
Jsh3ng <tanjs41200@gmail.com>
2022-12-13 20:50:28 +0800

    Add url param reader to SortPostCard

--- src/components/postcomponents/SortPostCard.jsx
+++ src/components/postcomponents/SortPostCard.jsx
@@ -1,68 +1,82 @@
 import { FireOutlined, HistoryOutlined, StarOutlined } from "@ant-design/icons";
 import { Button, Card, Col, Row, Select, Space } from "antd";
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const SortPostCard = ({ width, height }) => {
+  const [queryParams] = useSearchParams(window.location.search); //FOR TESTING
+  const TEST = queryParams.get("sort"); //FOR TESTING
   const buttonWidth = "75px";
 
   return (
-    <Card
-      style={{
-        size: "small",
-        margin: "10px auto",
-        padding: "10px",
-        height: height,
-        width: width,
-      }}
-      bodyStyle={{
-        padding: "0",
-      }}
-    >
-      <Row align="middle">
-        <Col>
-          <Space
-            size={10}
-            style={{
-              margin: "0px 10px 0px 0px",
-            }}
-          >
-            <Button
-              icon={<FireOutlined />}
+    <>
+      {/* FOR TESTING ---> */}
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        This is the value of postId taken from the URL is: {TEST}
+      </div>
+      {/* <--- FOR TESTING */}
+      <Card
+        style={{
+          size: "small",
+          margin: "10px auto",
+          padding: "10px",
+          height: height,
+          width: width,
+        }}
+        bodyStyle={{
+          padding: "0",
+        }}
+      >
+        <Row align="middle">
+          <Col>
+            <Space
+              size={10}
               style={{
-                width: buttonWidth,
+                margin: "0px 10px 0px 0px",
               }}
             >
-              Hot
-            </Button>
-            <Button
-              icon={<StarOutlined />}
+              <Button
+                icon={<FireOutlined />}
+                style={{
+                  width: buttonWidth,
+                }}
+              >
+                Hot
+              </Button>
+              <Button
+                icon={<StarOutlined />}
+                style={{
+                  width: buttonWidth,
+                }}
+              >
+                New
+              </Button>
+              <Button
+                icon={<HistoryOutlined />}
+                style={{
+                  width: buttonWidth,
+                }}
+              >
+                Old
+              </Button>
+            </Space>
+          </Col>
+          <Col flex="auto">
+            <Select
+              mode="tags"
+              placeholder="Sort by Tags"
               style={{
-                width: buttonWidth,
+                width: "100%",
               }}
-            >
-              New
-            </Button>
-            <Button
-              icon={<HistoryOutlined />}
-              style={{
-                width: buttonWidth,
-              }}
-            >
-              Old
-            </Button>
-          </Space>
-        </Col>
-        <Col flex="auto">
-          <Select
-            mode="tags"
-            placeholder="Sort by Tags"
-            style={{
-              width: "100%",
-            }}
-          />
-        </Col>
-      </Row>
-    </Card>
+            />
+          </Col>
+        </Row>
+      </Card>
+    </>
   );
 };
 
