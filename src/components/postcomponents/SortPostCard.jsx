e4611bc02fc8a636c846707198eff6b2ff78a3f6
Jsh3ng <tanjs41200@gmail.com>
2022-12-13 17:44:56 +0800

    Remove unneccessary abstraction of button

--- src/components/postcomponents/SortPostCard.jsx
+++ src/components/postcomponents/SortPostCard.jsx
@@ -2,80 +2,68 @@ import { FireOutlined, HistoryOutlined, StarOutlined } from "@ant-design/icons";
 import { Button, Card, Col, Row, Select, Space } from "antd";
 import React from "react";
 
-const SortNewButton = () => (
-  <Button
-    icon={<StarOutlined />}
-    style={{
-      width: "75px",
-    }}
-  >
-    New
-  </Button>
-);
+const SortPostCard = ({ width, height }) => {
+  const buttonWidth = "75px";
 
-const SortOldButton = () => (
-  <Button
-    icon={<HistoryOutlined />}
-    style={{
-      width: "75px",
-    }}
-  >
-    Old
-  </Button>
-);
-
-const SortBestButton = () => (
-  <Button
-    icon={<FireOutlined />}
-    style={{
-      width: "75px",
-    }}
-  >
-    Hot
-  </Button>
-);
-
-const SelectTag = () => (
-  <Select
-    mode="tags"
-    placeholder="Sort by Tags"
-    style={{
-      width: "100%",
-    }}
-  />
-);
-
-const SortPostCard = ({ width, height }) => (
-  <Card
-    style={{
-      size: "small",
-      margin: "10px auto",
-      padding: "10px",
-      height: height,
-      width: width,
-    }}
-    bodyStyle={{
-      padding: "0",
-    }}
-  >
-    <Row align="middle">
-      <Col>
-        <Space
-          size={10}
-          style={{
-            margin: "0px 10px 0px 0px",
-          }}
-        >
-          <SortBestButton />
-          <SortNewButton />
-          <SortOldButton />
-        </Space>
-      </Col>
-      <Col flex="auto">
-        <SelectTag />
-      </Col>
-    </Row>
-  </Card>
-);
+  return (
+    <Card
+      style={{
+        size: "small",
+        margin: "10px auto",
+        padding: "10px",
+        height: height,
+        width: width,
+      }}
+      bodyStyle={{
+        padding: "0",
+      }}
+    >
+      <Row align="middle">
+        <Col>
+          <Space
+            size={10}
+            style={{
+              margin: "0px 10px 0px 0px",
+            }}
+          >
+            <Button
+              icon={<FireOutlined />}
+              style={{
+                width: buttonWidth,
+              }}
+            >
+              Hot
+            </Button>
+            <Button
+              icon={<StarOutlined />}
+              style={{
+                width: buttonWidth,
+              }}
+            >
+              New
+            </Button>
+            <Button
+              icon={<HistoryOutlined />}
+              style={{
+                width: buttonWidth,
+              }}
+            >
+              Old
+            </Button>
+          </Space>
+        </Col>
+        <Col flex="auto">
+          <Select
+            mode="tags"
+            placeholder="Sort by Tags"
+            style={{
+              width: "100%",
+            }}
+          />
+        </Col>
+      </Row>
+    </Card>
+  );
+};
 
 export default SortPostCard;
