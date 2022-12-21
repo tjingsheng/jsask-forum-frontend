682334ba0bf160a93ca6905968f28fbce6689f97
Jsh3ng <tanjs41200@gmail.com>
2022-12-22 00:27:50 +0800

    Add route for login page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/home";
+import LoginPage from "../pages/login";
 import NotFoundPage from "../pages/notfound";
 import PostPage from "../pages/post";
 
 const routes = createBrowserRouter([
   {
     path: "/",
+    element: <LoginPage />,
     errorElement: <NotFoundPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
   },
   {
     path: "home",
