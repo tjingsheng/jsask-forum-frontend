33063a0e7d08836f24f22870cc8af58ab097deed
Jsh3ng <tanjs41200@gmail.com>
2022-12-22 17:22:45 +0800

    Remove old AvatarIcon.jsx

--- src/layouts/components/headercomponents/HeaderAvatarIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { UserOutlined } from "@ant-design/icons";
-import { Avatar } from "antd";
-import React from "react";
-
-const AvatarIcon = () => (
-  <Avatar size="large" icon={<UserOutlined />} shape="square" />
-);
-
-export default AvatarIcon;
