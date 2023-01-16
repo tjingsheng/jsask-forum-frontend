775bc8d919bd29cd01c44af33ceeee54e640ca28
Jsh3ng <tanjs41200@gmail.com>
2023-01-16 11:25:17 +0800

    Fix typo

--- src/redux/sagas/AuthenticationSaga.js
+++ src/redux/sagas/AuthenticationSaga.js
@@ -20,7 +20,8 @@ function* loginProcess(action) {
   try {
     const requestURI = URI.getCurrUser + "/" + action.payload.username;
     const response = yield axiosRequest(RequestMethod.get, requestURI);
-    yield put(authenticationAction.loginSuccess(response.data.payload.data));
+    const currUser = response.data.payload.data;
+    yield put(authenticationAction.loginSuccess(currUser));
   } catch (e) {
     console.log(e);
     yield put(authenticationAction.loginFailed());
