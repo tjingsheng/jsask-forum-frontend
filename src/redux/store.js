6adb32eeef867bf27507f8afc94e4c5d3616eccb
Jsh3ng <tanjs41200@gmail.com>
2023-01-21 16:03:42 +0800

    Add state persist function

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,37 @@ import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
+const saveStateToLocalStorage = (state) => {
+  try {
+    localStorage.setItem("savedState", JSON.stringify(state));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const loadStateFromLocalStorage = () => {
+  try {
+    const stateStr = localStorage.getItem("savedState");
+    return stateStr ? JSON.parse(stateStr) : undefined;
+  } catch (e) {
+    console.error(e);
+    return undefined;
+  }
+};
+
+const persistedStore = loadStateFromLocalStorage();
+
 const store = configureStore({
   reducer: rootReducer,
   middleware: () => [sagaMiddleware],
   devTools:
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__(),
+  preloadedState: persistedStore,
+});
+
+store.subscribe(() => {
+  saveStateToLocalStorage(store.getState());
 });
 
 sagaMiddleware.run(rootSaga);
