d232b87ceed6b81356511201c84f26028e7386c2
tjingsheng <tanjs41200@gmail.com>
2023-01-24 13:25:52 +0800

    Format

--- src/assets/JsaskSpin.jsx
+++ src/assets/JsaskSpin.jsx
@@ -3,7 +3,6 @@ import JsaskLogo from "./JsaskLogo";
 
 const JsaskSpin = ({ ...props }) => {
   const customIcon = <JsaskLogo id="loading" style={{ fontSize: "10vw", top: "10vh", left: "45vw" }} />;
-
   return <Spin indicator={customIcon} {...props} />;
 };
 export default JsaskSpin;
